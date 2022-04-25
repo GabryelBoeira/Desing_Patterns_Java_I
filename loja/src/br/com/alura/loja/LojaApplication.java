1ff4c09a2a30806dfa886efe03a923688a717a87
GabryelBoeira <gabryel@asaplog.com.br>
2022-04-25 14:28:28 -0300

    Criando codigo base para utilizando o pattern Strategy

--- loja/src/br/com/alura/loja/LojaApplication.java
+++ loja/src/br/com/alura/loja/LojaApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja;
 
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.tipoImposto.ICMS;
 import br.com.alura.loja.imposto.tipoImposto.ISS;
@@ -10,11 +11,22 @@ import java.math.BigDecimal;
 public class LojaApplication {
 
     public static void main(String[] args) {
+        CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+        CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
 
-        Orcamento orcamento = new Orcamento(new BigDecimal("150"));
-        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+        Orcamento orcamento = new Orcamento(new BigDecimal("150"), 6);
+        Orcamento orcamento2 = new Orcamento(new BigDecimal("250"), 6);
+        Orcamento orcamento3 = new Orcamento(new BigDecimal("550"), 3);
 
-        System.out.println("ISS: " + calculadora.calcular(orcamento, new ISS()));
-        System.out.println("ICMS: " + calculadora.calcular(orcamento, new ICMS()));
+        System.out.println("======================");
+        System.out.println("CalculadoraDeImpostos");
+        System.out.println("ISS: " + calculadoraDeImpostos.calcular(orcamento, new ISS()));
+        System.out.println("ICMS: " + calculadoraDeImpostos.calcular(orcamento, new ICMS()));
+
+        System.out.println("======================");
+        System.out.println("CalculadoraDeDesconto");
+        System.out.println("Desconto por Qtde de produto: " + calculadoraDeDescontos.calcularDesconto(orcamento2));
+        System.out.println("Desconto por valor total: " + calculadoraDeDescontos.calcularDesconto(orcamento3));
+
     }
 }
--- loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -5,9 +5,18 @@ import java.math.BigDecimal;
 public class Orcamento {
 
     private BigDecimal valor;
+    private int qtdeItens;
 
-    public Orcamento(BigDecimal valor) {
+    public Orcamento(BigDecimal valor, int qtdeItens) {
         this.valor = valor;
+        this.qtdeItens = qtdeItens;
+    }
+    public int getQtdeItens() {
+        return qtdeItens;
+    }
+
+    public void setQtdeItens(int qtdeItens) {
+        this.qtdeItens = qtdeItens;
     }
 
     public BigDecimal getValor() {
