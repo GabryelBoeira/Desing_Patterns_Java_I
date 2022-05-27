b12f769a704586788aa57d743bde21f0edcac689
Gabryel Boeira <33075550+GabryelBoeira@users.noreply.github.com>
2022-05-27 17:52:04 -0300

    nova regara para trabalhar com Pattern State
    Adicionado nova regra de desconto baseada no situação na qual o orçamento se encontra atualmente

--- loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,11 +6,27 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int qtdeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int qtdeItens) {
         this.valor = valor;
         this.qtdeItens = qtdeItens;
     }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+        if (situacao.equals("EM_ANALISE"))
+            valorDoDescontoExtra= new BigDecimal("0.05");
+        else if (situacao.equals("APROVADO"))
+            valorDoDescontoExtra = new BigDecimal("0.02");
+
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
+    }
+
     public int getQtdeItens() {
         return qtdeItens;
     }
