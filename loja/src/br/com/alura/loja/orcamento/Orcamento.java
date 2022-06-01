6c303abb9951c659b2d8ee18e1e32c9e86da5180
Gabryel Boeira <33075550+GabryelBoeira@users.noreply.github.com>
2022-06-01 18:14:03 -0300

    Criado classe abstrata para gerenciar os estados
    Para reduzir a utilização de ifs no código utiliza-se o padrão State, praticamente igual ao padrão Strategy, sua utilização e voltada para gerenciar estados de um o objeto aonde o mesmo não permita retornar a qualquer estado anterior a aquele no qual se encontra o objeto

--- loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,7 +6,7 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int qtdeItens;
-    private String situacao;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int qtdeItens) {
         this.valor = valor;
@@ -14,17 +14,29 @@ public class Orcamento {
     }
 
     public void aplicarDescontoExtra() {
-        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
-        if (situacao.equals("EM_ANALISE"))
-            valorDoDescontoExtra= new BigDecimal("0.05");
-        else if (situacao.equals("APROVADO"))
-            valorDoDescontoExtra = new BigDecimal("0.02");
 
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
         this.valor = this.valor.subtract(valorDoDescontoExtra);
     }
 
     public void aprovar() {
-        this.situacao = "APROVADO";
+        this.situacao.aprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 
     public int getQtdeItens() {
