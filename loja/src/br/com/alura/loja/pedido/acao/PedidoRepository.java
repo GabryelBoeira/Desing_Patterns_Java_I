c91eb52da716a668402f6133eba2f341b463eecf
gabryelboeira <gabryel392@gmail.com>
2022-06-05 14:51:21 -0300

    criando classes de acao para o pedido

---
+++ loja/src/br/com/alura/loja/pedido/acao/PedidoRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja.pedido.acao;
+
+import br.com.alura.loja.pedido.Pedido;
+
+public class PedidoRepository {
+
+    public void executar(Pedido pedido) {
+        System.out.println("Salvar pedido no banco de dados!");
+    }
+}
